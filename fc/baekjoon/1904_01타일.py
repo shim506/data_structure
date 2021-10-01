f6e7a9ce677c48351d7ad7442dbb6e091cf8fa2e
sim <shim506@naver.com>
2021-10-01 23:31:40 +0900

    no message

--- fc/baekjoon/1904_01타일.py
+++ fc/baekjoon/1904_01타일.py
@@ -1,16 +1,6 @@
 # 01 타일
 
-N = int(input())
 
-lst =[0] * 1000001
-
-lst[1] = 1
-lst[2] = 2
-
-for i in range(3 , N+1):
-    lst[i] = (lst[i-1] + lst[i-2]) %15746
-
-print(lst[N] )
 '''
 
 def tile(x : int):
