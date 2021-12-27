0417da0c971750951474e49a9970d577d3d7243a
sim <shim506@naver.com>
2021-12-27 19:31:27 +0900

    백준 문제풀이 kotlin 단어공부 1157 배열

--- kotlin/단어공부1157/src/Main.kt
+++ kotlin/단어공부1157/src/Main.kt
@@ -1,22 +1,26 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.*
+import java.util.*
+import kotlin.collections.HashMap
 
-fun main()=with(BufferedReader(InputStreamReader(System.`in`))){
 
-    val word=readLine().toCharArray()
-    var map=mutableMapOf<Char, Int>()
-    word.forEach{
-        if(map.contains(it.toUpperCase())){
-            map[it.toUpperCase()]=map.get(it.toUpperCase())!!+1
-        }else{
-            map[it.toUpperCase()]=1
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+
+    val word = readLine().toCharArray()
+    var map = mutableMapOf<Char, Int>()
+    word.forEach {
+        if (map.contains(it.toUpperCase())) {
+            map[it.toUpperCase()] = map.get(it.toUpperCase())!! + 1
+        } else {
+            map[it.toUpperCase()] = 1
         }
     }
-    val max=map.maxByOrNull{it.value}
+    val max = map.maxByOrNull { it.value }
 
-    if(map.filter{it.value==max!!.value}.count()>1){
+    if (map.filter { it.value == max!!.value }.count() > 1) {
         println("?")
-    }else{
+    } else {
         println(max!!.key)
     }
+
+
 }
