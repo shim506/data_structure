afdd7481e6cda25c994be5eca738ea021d10b280
sim <shim506@naver.com>
2022-01-24 23:57:53 +0900

    스티커

--- kotlin/9465_스티커/src/Main.kt
+++ kotlin/9465_스티커/src/Main.kt
@@ -1,5 +1,6 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
 import kotlin.math.max
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
@@ -7,25 +8,29 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
 
 // 해당위치까지 올 수 있는 값들을 누적하여 해당 위의 값을 결정한다.
     repeat(T) {
+
         val N = readLine().toInt()
         val arr = Array<Array<Int>>(2) { Array<Int>(N + 1) { 0 } }
         val dp = Array<Array<Int>>(2) { Array<Int>(N + 1) { 0 } }
 
-        val arr1 = readLine().split(" ").map { it.toInt() }
-        val arr2 = readLine().split(" ").map { it.toInt() }
-
-        arr[0] = arr1.toTypedArray()
-        arr[1] = arr2.toTypedArray()
-
-        var colNum = -1
-        var rowNum = 0
-        while (colNum != N) {
-            dp[]
-
+        for (i in 0..1) {
+            var st = StringTokenizer(readLine(), " ")
+            for (j in 1..N) {
+                arr[i][j] = st.nextToken().toInt()
+            }
         }
 
+        if (N >= 1) {
+            dp[0][1] = arr[0][1]
+            dp[1][1] = arr[1][1]
+        }
 
-        println(max(arr[0][N], arr[1][N]))
+        for (i in 2..N) {
+            dp[0][i] = arr[0][i] + max(dp[1][i - 2], dp[1][i - 1])
+            dp[1][i] = arr[1][i] + max(dp[0][i - 2], dp[0][i - 1])
+        }
+
+        println(max(dp[0][N], dp[1][N]))
     }
 
 
