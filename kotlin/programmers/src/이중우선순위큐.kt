a9b5076e2e27831191b0ab0224fdf74937edd55e
shim506 <shim506@naver.com>
2022-05-07 00:28:00 +0900

    힙 : 프로그래머스_이중우선순위큐 -> 수정

--- kotlin/programmers/src/이중우선순위큐.kt
+++ kotlin/programmers/src/이중우선순위큐.kt
@@ -1,29 +1,27 @@
 import java.util.PriorityQueue
 import java.util.Collections
-import java.util.HashMap
 
 
 class Solution이중우선순위큐 {
+
     // 최대힙과 최소힙 두개를 관리한다.
-    // 최후에는 두개의 중복 값을 통해 리스트를 완성한다
     fun solution(operations: Array<String>): IntArray {
-        var answer = mutableListOf<Int>()
 
         val minQueue = PriorityQueue<Int>()
         val maxQueue = PriorityQueue<Int>(Collections.reverseOrder())
-
 
         operations.forEach {
             val input = it.split(" ")
             if (input[0] == "I") {
                 minQueue.add(input[1].toInt())
-            } else if (input[1] == "1") {
-                maxQueue.poll()
-            } else minQueue.poll()
+                maxQueue.add(input[1].toInt())
+            } else if (minQueue.size == 0)
+            else if (input[1] == "1") minQueue.remove(maxQueue.poll())
+            else maxQueue.remove(minQueue.poll())
         }
 
-        answer.addAll(minQueue.filter { maxQueue.contains(it) })
-        
-        return answer.toIntArray()
+
+        return if (maxQueue.size > 0) intArrayOf(maxQueue.peek(), minQueue.peek())
+        else intArrayOf(0, 0)
     }
 }
